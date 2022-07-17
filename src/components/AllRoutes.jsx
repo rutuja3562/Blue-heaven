e4852672650c49c7b5be33b2cb7a107adac4becb
rutuja3562 <rspatil3562@gmail.com>
2022-07-17 20:17:23 +0530

    Routes added

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import { Cart } from './Cart';
+import { Checkout } from './Checkout';
 import { Home } from './Home'
 import { Product } from './Product';
 import { ProductDetails } from './ProductDetails';
-
+import {Payment} from "./Payment"
 export const AllRoutes = () => {
   return (
     <div>
@@ -12,7 +13,9 @@ export const AllRoutes = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/product" element={<Product />}></Route>
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/payment" element={<Payment />}></Route>
       </Routes>
     </div>
   );
